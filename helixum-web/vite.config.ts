e7e74f4a40496c06c42cfda7f89923f5d4ef53ba
Nicholas Fane <nfane@Nicholass-MBP.modem>
2025-08-03 13:08:26 +1000

    adding boiletplate project structure and future aliases for them

--- helixum-web/vite.config.ts
+++ helixum-web/vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@app': '/src/app',
+      '@components': '/src/components',
+      '@features': '/src/features',
+      '@hooks': '/src/hooks',
+      '@lib': '/src/lib',
+      '@types': '/src/types',
+      '@utils': '/src/utils',
+      '@providers': '/src/providers',
+      '@layouts': '/src/layouts',
+      '@pages': '/src/pages',
+      '@services': '/src/services',
+      '@store': '/src/store',
+      '@assets': '/src/assets',
+    },
+  },
 })
