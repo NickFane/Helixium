c7b6bdb757719022214de691d2eb3c86c2232fbd
Nicholas Fane <nfane@Nicholass-MBP.modem>
2025-08-13 15:08:03 +1000

    tf format

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -106,10 +106,10 @@ resource "aws_security_group" "ecs" {
   vpc_id      = aws_vpc.helixium.id
 
   ingress {
-    description = "HTTP from ALB"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
 
